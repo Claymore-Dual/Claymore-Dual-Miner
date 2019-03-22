4f1055d0bc070c982577da0df8e7d0085a7e878a
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 00:24:03 +0300

    Update ethereum-nanopool.bat

--- ethereum-nanopool.bat
+++ ethereum-nanopool.bat
@@ -6,6 +6,7 @@ set GPU_MAX_ALLOC_PERCENT=100
 set GPU_SINGLE_ALLOC_PERCENT=100
 @echo on
 
+
 EthDcrMiner64.exe -epool eth-eu2.nanopool.org:9999 -ewal 0x1a0e2c4cd699cee12672adc223fdb30b93253eba -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -tt 73 -ttli 79 -tstop 82 -fanmin 30
 pause
 
