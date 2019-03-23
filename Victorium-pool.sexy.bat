bba969b68614c932c422fb5eee86b93901d8727c
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:46:11 +0300

    Update Victorium-pool.sexy.bat

--- Victorium-pool.sexy.bat
+++ Victorium-pool.sexy.bat
@@ -9,3 +9,4 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 EthDcrMiner64.exe -epool vic.pool.sexy:10062 -ewal 0x05c6957d4b39cc617f723f2c4d84612bf74f787b -eworker rig1 -epsw x -esm 0 -allcoins 1 -allpools 1 -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 70 -ttli 79 -tstop 82 -fanmin 30
 
 pause
+
