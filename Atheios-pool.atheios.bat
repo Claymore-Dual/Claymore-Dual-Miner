75b21827fd02d3a62ccfdf4d608634894c02972c
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:43:36 +0300

    Update Atheios-pool.atheios.bat

--- Atheios-pool.atheios.bat
+++ Atheios-pool.atheios.bat
@@ -9,3 +9,4 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 EthDcrMiner64.exe -epool pool.atheios.com:8007 -ewal 0x424c6e4bbbd328e4b960a6c2eaa93bbdabeb5b6a -eworker rig1 -epsw x -esm 0 -allcoins 1 -allpools 1 -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 70 -ttli 79 -tstop 82 -fanmin 30
 
 pause
+
