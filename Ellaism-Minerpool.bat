1e871d13ce006d5f9b3b45d416ae66430e07d57b
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:44:32 +0300

    Update Ellaism-Minerpool.bat

--- Ellaism-Minerpool.bat
+++ Ellaism-Minerpool.bat
@@ -8,4 +8,5 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 
 EthDcrMiner64.exe -epool ella.minerpool.net:8002 -ewal 0x98a421d19f929ad957e1baa5ea80702774550dc6 -eworker rig1 -epsw x -allcoins 1 -allpools 1 -gser 2 -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 73 -ttli 79 -tstop 82 -fanmin 30
 
+
 pause
