4b7922d9f05eaccb3322559e83e6e0f7755ede4f
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:46:05 +0300

    Update WhaleCoin-minerpool.bat

--- WhaleCoin-minerpool.bat
+++ WhaleCoin-minerpool.bat
@@ -1,2 +1,3 @@
 EthDcrMiner64.exe -epool stratum+tcp://eu.whalecoin.eu:8001 -ewal 0xb32242f822371b66ee588c21e1af5db7645f79f4 -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 78 -ttli 79 -tstop 85 -fanmin 30 -allcoins 1
 pause
+
