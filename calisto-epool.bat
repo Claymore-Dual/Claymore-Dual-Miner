3ad7bb22982d0d78f2063bcde1999cc8a9bbf00d
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:46:00 +0300

    Update calisto-epool.bat

--- calisto-epool.bat
+++ calisto-epool.bat
@@ -1,2 +1,3 @@
 EthDcrMiner64.exe -epool stratum+tcp://eu.clo.epool.io:8008 -ewal 0xfed8e9b8ae81c863cc8a987cb5b21eed4e8b43a7 -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 78 -ttli 79 -tstop 85 -fanmin 30 -allcoins 1
 pause
+
