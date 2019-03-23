965ee98b8de43fabae4f8f03d593cd95fa8b881f
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:44:16 +0300

    Update MIX-Blockchain-minerpool.bat

--- MIX-Blockchain-minerpool.bat
+++ MIX-Blockchain-minerpool.bat
@@ -1,2 +1,3 @@
 EthDcrMiner64.exe -epool stratum+tcp://mix.minerpool.net:8002 -ewal 0x6b7b0321c7d7b8bca717a41b15f1d574b7fbd2e0 -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 78 -ttli 79 -tstop 85 -fanmin 30 -allcoins 1
 pause
+
