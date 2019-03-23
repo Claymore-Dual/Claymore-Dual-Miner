7ce9a0115a83945fdd742b409ad3901fbd0cf333
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:46:44 +0300

    Update Nilu-minerpool.bat

--- Nilu-minerpool.bat
+++ Nilu-minerpool.bat
@@ -9,3 +9,4 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 EthDcrMiner64.exe -epool nilu.minerpool.net:8001 -ewal 0x41907b3ccaa4ca9ad2186c58bdb7a170b31be473 -eworker rig1 -epsw x -esm 0 -allcoins 1 -allpools 1 -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 70 -ttli 79 -tstop 82 -fanmin 30
 
 pause
+
