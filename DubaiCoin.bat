202cfddbee3b2bfd01cf420ef10327429a8ee24a
Dual-miner <48830617+Dual-miner@users.noreply.github.com>
2019-03-23 18:44:37 +0300

    Update DubaiCoin.bat

--- DubaiCoin.bat
+++ DubaiCoin.bat
@@ -1,2 +1,3 @@
 EthDcrMiner64.exe -epool stratum+tcp://dbix.pool.sexy:10032 -ewal 0xe9011c5d954323dab1de59fd73b2146057a0d254 -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 78 -ttli 79 -tstop 85 -fanmin 30 -allcoins 1
 pause
+
