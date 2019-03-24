4dedd5c79df6a3ae4c2961e28cc2bf0407869c00
Claymore-Dual-Ethereum-GPU-Miner-12 <48881727+Claymore-Dual-Ethereum-GPU-Miner-12@users.noreply.github.com>
2019-03-24 15:12:24 +0300

    Update dual-ethereum-verge-dwarfpool-unimining.bat

--- dual-ethereum-verge-dwarfpool-unimining.bat
+++ dual-ethereum-verge-dwarfpool-unimining.bat
@@ -8,4 +8,6 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 
 EthDcrMiner64.exe -epool eth-ru2.dwarfpool.com:8008 -ewal 0x1a0e2c4cd699cee12672adc223fdb30b93253eba.rig1 -epsw x -etha 0 -dpool xvg.eu1.unimining.net:5770 -dwal DPNSdySRg8zFmU6Zcqyo9XKJKDhMm87Xn7.rig1 -dpsw c=XVG -dcoin blake2s -r 1 -dbg -1 -mport 0 -retrydelay 3 -ftime 45 -tt 88 -ttli 88 -tstop 88 -fanmin 30 -dcri 25
 
+
+
 pause
