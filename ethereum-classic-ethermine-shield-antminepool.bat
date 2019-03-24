2b84166770743c39c456e117c1b8609c2b485ea1
Claymore-Dual-Ethereum-Miner-12 <48881675+Claymore-Dual-Ethereum-Miner-12@users.noreply.github.com>
2019-03-24 15:19:28 +0300

    Update ethereum-classic-ethermine-shield-antminepool.bat

--- ethereum-classic-ethermine-shield-antminepool.bat
+++ ethereum-classic-ethermine-shield-antminepool.bat
@@ -8,4 +8,5 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 
 EthDcrMiner64.exe -epool eu1-etc.ethermine.org:4444 -ewal 0xfcf2ba59016a4b2aff4097feba1d927deefcb5d4.rig1 -epsw x -allcoins etc -allpools 1 -gser 2 -dpool s.antminepool.com:5766 -dwal SX66Mvuy5dLQo2rdTxHueyAJFXTwHDQzpj.rig1 -dpsw c=XSH -dcoin blake2s -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 73 -ttli 79 -tstop 86 -fanmin 30 -dcri 45
 
+
 pause
