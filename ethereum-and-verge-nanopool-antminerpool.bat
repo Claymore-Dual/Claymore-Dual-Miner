4a717dc1bfba3a2d069f60b185bc1a611c155d85
Claymore-Dual-Ethereum-GPU-Miner <48881704+Claymore-Dual-Ethereum-GPU-Miner@users.noreply.github.com>
2019-03-24 15:18:47 +0300

    Update ethereum-and-verge-nanopool-antminerpool.bat

--- ethereum-and-verge-nanopool-antminerpool.bat
+++ ethereum-and-verge-nanopool-antminerpool.bat
@@ -6,6 +6,7 @@ set GPU_MAX_ALLOC_PERCENT=100
 set GPU_SINGLE_ALLOC_PERCENT=100
 @echo on
 
+
 EthDcrMiner64.exe -epool eth-eu2.nanopool.org:9999 -ewal 0x1a0e2c4cd699cee12672adc223fdb30b93253eba -eworker rig1 -epsw x -etha 0 -allpools 1 -dpool stratum+tcp://s.antminepool.com:9009 -dwal DDxgi1akA5ewepRdyznzL3rS1ssmjTvVxr -dpsw c=XVGB -dcoin blake2s -r 1 -dbg -1 -mport 0 -retrydelay 3 -ftime 45 -tt 73 -ttli 79 -tstop 82 -fanmin 30 -dcri 50
 
 pause
