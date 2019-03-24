07b3be424e773e2376431a268f72d6afdf207c09
Claymores-Dual <48881566+Claymores-Dual@users.noreply.github.com>
2019-03-24 15:20:13 +0300

    Update ethereum-classic-ethermine.bat

--- ethereum-classic-ethermine.bat
+++ ethereum-classic-ethermine.bat
@@ -8,5 +8,6 @@ set GPU_SINGLE_ALLOC_PERCENT=100
 
 EthDcrMiner64.exe -epool eu1-etc.ethermine.org:4444 -ewal 0xfcf2ba59016a4b2aff4097feba1d927deefcb5d4.rig1 -epsw x -allpools 1 -allcoins etc -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 70 -ttli 79 -tstop 82 -fanmin 30
 
+
 pause
 
