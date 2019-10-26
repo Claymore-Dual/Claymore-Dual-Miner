9be6f4f3bca2e86d90a36d87e6d4c6495e80f0c4
crazypool2019 <45013538+crazypool2019@users.noreply.github.com>
2019-10-26 13:46:02 -0300

    Create ethereum-crazypool.bat
    PPLNS CrazyPool.org

---
+++ BAT files/ethereum-crazypool.bat
@@ -0,0 +1,2 @@
+EthDcrMiner64.exe -epool eu.crazypool.org:7011 -ewal 0x47cb11428a97e543b58a6d926bf2e1846042775d -eworker rig1 -epsw x -mode 1 -r 1 -dbg -1 -mport 0 -etha 0 -retrydelay 3 -ftime 45 -tt 70 -ttli 79 -tstop 82 -fanmin 30
+pause
